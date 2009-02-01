0fd88bb163a06269524beb097cf1b2d71dd0cf0b
funzoneq2 <funzoneq2@91c8e7d8-2d28-0410-ae74-2b12f4eb5758>
2009-02-01 12:23:23 +0000

    new invoice template.

--- trunk/FreshInvoice/templates/factuur.sample.tpl.php
+++ trunk/FreshInvoice/templates/factuur.sample.tpl.php
@@ -4,80 +4,69 @@
 <title>Factuur</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <style>
-body {
-	font-family: tahoma;
-	font-size: 11px;
-}
-.bold {
-	font-family: tahoma;
-	font-size: 11px;
-	font-weight: bold;
-}
+body { font-family: tahoma; font-size: 12px; }
+.bold { font-weight: bold; }
+#address { position:relative; left: 50px; top: 50px; width: 250px; font-weight: bold; }
+#invoiceinfo { position:relative; left: 0px; top: 110px; width: 350px; }
+#invoice { position:relative; left: 0px; top: -50px; width: 100%; }
+#contactinfo { position:relative; align: right; width: 175px; left: 85%; top: -250px; }
+#bankinfo { position:relative; align: right; width: 175px; left: 85%; top: -20px; }
 </style>
 </head>
 
 <body>
-{#LOGO#}<br /><br />
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<div id="container">
+
+<div id="logo">{#LOGO#}</div>
+
+<div id="address">{#KLANTBEDRIJFSNAAM#}{#KLANTNAAM#}<br />
+{#KLANTADRES#}</div>
+
+
+<div id="invoiceinfo">
+<table width="100%"  border="0" cellspacing="0" cellpadding="0">
   <tr>
-    <td width="27%" valign="top"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
-        <tr>
-          <td width="50%">Factuurnummer:</td>
-          <td>{#INVOICEPREPEND#}{#FACTUURID#}</td>
-        </tr>
-        <tr>
-          <td>Datum:</td>
-          <td>{#FACTUURDATUM#}</td>
-        </tr>
-        <tr>
-          <td>Uiterste betaaldatum:</td>
-          <td>{#INVOICEEXPIREDATE#}</td>
-        </tr>
-        <tr>
-          <td>Klantnummer:</td>
-          <td>{#KLANTID#}</td>
-        </tr>
-    </table><br />
-    ABN Amro te Amsterdam<br />
-    <strong>11.11.11.111</strong><br />
-    BIC ABNANL2A<br />
-    IBAN NL74ABNA05111111111</td>
-    <td width="45%" valign="top">&nbsp;</td>
-    <td width="28%" valign="top"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
-        <tr align="right">
-          <td valign="top">Sample business<br />
-            Random address 21<br />
-            1111 AB Amsterdam<br /><br />
-            KvK nummer: 1111111 te Amsterdam<br />
-            BTW nummer: NL0000.00.000.B01<br />
-			tel. 0031 (0)6 11 11 11 11<br />
-			fax  0031 (0)84 11 11 111<br />
-      		e-mail. info@example.com</td>
-        </tr>
-        </table></td>
+    <td width="50%">Factuurnummer:</td>
+    <td>{#INVOICEPREPEND#}{#FACTUURID#}</td>
+  </tr>
+  <tr>
+	<td>Datum:</td>
+	<td>{#FACTUURDATUM#}</td>
+  </tr>
+  <tr>
+	<td>Uiterste betaaldatum:</td>
+	<td>{#INVOICEEXPIREDATE#}</td>
+  </tr>
+  <tr>
+	<td>Klantnummer:</td>
+	<td>{#KLANTID#}</td>
   </tr>
 </table>
-<br /><br />
-<table width="100%" border="0" cellpadding="0" cellspacing="0" class="bold">
-  <tr>
-    <td width="15%">&nbsp;</td>
-    <td width="25%">
-	  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-		<tr>
-          <td>{#KLANTBEDRIJFSNAAM#}{#KLANTNAAM#}<br />
-		  {#KLANTADRES#}</td>
-        </tr>
-      </table></td>
-    <td width="60%">&nbsp;</td>
-  </tr>
-</table>
-<br /><br />
+</div>
+
+<div id="bankinfo">
+ABN Amro te Amsterdam<br />
+<strong>11.11.11.111</strong><br />
+BIC ABNANL2A<br />
+IBAN NL74ABNA05111111111</div>
+
+<div id="contactinfo">Sample business<br />
+Random address 21<br />
+1111 AB Amsterdam<br /><br />
+KvK nummer: 1111111 te Amsterdam<br />
+BTW nummer: NL0000.00.000.B01<br />
+tel. 0031 (0)6 11 11 11 11<br />
+fax  0031 (0)84 11 11 111<br />
+e-mail. info@example.com</div>
+
+<div id="invoice">
 <table width="100%"  border="0" cellspacing="0" cellpadding="2" class="bold">
   <tr>
     <td width="50%">Product omschrijving</td>
     <td width="10%">Datum</td>
     <td width="10%">Periode</td>
     <td width="10%">Aantal</td>
+
     <td width="10%" align="right">Prijs</td>
     <td width="10%" align="right">Totaal</td>
   </tr>
@@ -104,5 +93,8 @@ body {
   <td width="10%" align="right">{#FACTUURTOTAAL#}</td>
 </tr>
 </table>
+</div>
+
+</div>
 </body>
 </html>
